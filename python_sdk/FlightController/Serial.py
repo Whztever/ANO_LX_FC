a5a42d420af07f0eec4607832be8904b85480862
Ellu <lutaoyu518@gmail.com>
2023-06-30 20:01:00 +0800

    🦄 refactor: 重写串口模块(Not Tested)

--- python_sdk/FlightController/Serial.py
+++ python_sdk/FlightController/Serial.py
@@ -1,4 +1,3 @@
-from copy import copy
 from sys import byteorder as sysByteorder
 
 import serial
@@ -6,100 +5,161 @@ import serial
 
 class FC_Serial:
     def __init__(self, port, baudrate, timeout=0.5, byteOrder=sysByteorder):
+        """
+        初始化方法，用于创建 FC_Serial 对象并打开串口连接。
+
+        Args:
+            port (str): 串口名称。
+            baudrate (int): 波特率。
+            timeout (float, optional): 超时时间。默认为 0.5 秒。
+            byteOrder (str, optional): 字节序。默认为系统字节序。
+
+        Returns:
+            None
+        """
         self.ser = serial.Serial(port, baudrate, timeout=timeout)
-        self.read_buffer = bytes()
-        self.read_save_buffer = bytes()
-        self.reading_flag = False
-        self.read_save_buffer = bytes()
-        self.pack_count = 0
-        self.pack_length = 0
-        self.byte_order = byteOrder
-        self.waiting_buffer = bytes()
+        self.data = bytes()
+        self._read_buffer = bytes()
+        self._in_waiting_buffer = bytes()
+        self._reading_flag = False
+        self._pack_count = 0
+        self._pack_length = 0
+        self._byte_order = byteOrder
         self.send_config()
         self.read_config()
 
     def send_config(self, startBit=[], optionBit=[]):
-        self.sned_start_bit = startBit
-        self.send_option_bit = optionBit
+        """
+        设置发送配置。
+
+        Args:
+            startBit (list, optional): 发送起始位。默认为空列表。
+            optionBit (list, optional): 发送选项位。默认为空列表。
+
+        Returns:
+            None
+        """
+        self._send_start_bit = startBit
+        self._send_option_bit = optionBit
+        self._send_head = bytes(self._send_start_bit) + bytes(self._send_option_bit)
 
     def read_config(self, startBit=[]):
-        self.read_start_bit = startBit
+        """
+        设置读取配置。
 
-    def check_rx_data_sum(self):
-        length = len(self.read_buffer)
+        Args:
+            startBit (list, optional): 读取起始位。默认为空列表。
+
+        Returns:
+            None
+        """
+        self._read_start_bit = startBit
+
+    def check_rx_data_checksum(self):
+        """
+        检查接收数据的校验和。
+
+        Returns:
+            int: 校验和是否正确。
+        """
+        length = len(self._read_buffer)
         checksum = 0
-        for i in self.read_start_bit:
+        for i in self._read_start_bit:
             checksum += i
             checksum &= 0xFF
-        checksum += self.pack_length_bit
+        checksum += self._pack_length_bit
         checksum &= 0xFF
         for i in range(0, length):
             checksum += int.from_bytes(
-                self.read_buffer[i : i + 1],
-                byteorder=self.byte_order,
+                self._read_buffer[i : i + 1],
+                byteorder=self._byte_order,
                 signed=False,
             )
             checksum &= 0xFF
-        received_checksum = int.from_bytes(self.ser.read(1), byteorder=self.byte_order, signed=False)
+        received_checksum = int.from_bytes(self.ser.read(1), byteorder=self._byte_order, signed=False)
         if received_checksum == checksum:
             return 1
         return 0
 
     def read(self) -> bool:
-        tmp = 0x00
+        """
+        读取串口数据。
+
+        Returns:
+            bool: 是否读取成功。True 表示读取成功，False 表示读取失败。
+        """
+        _len = len(self._read_start_bit)
         while self.ser.in_waiting > 0:
-            tmp = self.ser.read(1)
-            _len = len(self.read_start_bit)
-            if not self.reading_flag:
-                self.waiting_buffer += tmp
-                if len(self.waiting_buffer) >= _len:
-                    if self.waiting_buffer[-_len:] == bytes(self.read_start_bit):
-                        self.reading_flag = True
-                        self.read_buffer = bytes()
-                        self.pack_count = 0
-                        self.pack_length = -1
-                        self.waiting_buffer = bytes()
-                continue
-            if self.pack_length == -1:
-                self.pack_length_bit = int.from_bytes(tmp, self.byte_order, signed=False)  # type: ignore
-                self.pack_length = self.pack_length_bit & 0b11111111
-                continue
-            if self.reading_flag:
-                self.pack_count += 1
-                self.read_buffer += tmp
-                if self.pack_count >= self.pack_length:
-                    self.reading_flag = False
-                    if self.check_rx_data_sum():
-                        self.read_save_buffer = copy(self.read_buffer)
-                        self.read_buffer = bytes()
+            read_byte = self.ser.read(1)
+            if not self._reading_flag:
+                self._in_waiting_buffer += read_byte
+                if len(self._in_waiting_buffer) >= _len and self._in_waiting_buffer[-_len:] == self._read_start_bit:
+                    self._reading_flag = True
+                    self._read_buffer = bytes()
+                    self._pack_count = 0
+                    self._pack_length = -1
+                    self._in_waiting_buffer = bytes()
+            elif self._pack_length == -1:
+                self._pack_length_bit = int.from_bytes(read_byte, self._byte_order, signed=False)
+                self._pack_length = self._pack_length_bit & 0xFF
+            else:
+                self._pack_count += 1
+                self._read_buffer += read_byte
+                if self._pack_count >= self._pack_length:
+                    self._reading_flag = False
+                    checksum = sum(self._read_buffer[:-1]) & 0xFF
+                    checksum += self._pack_length_bit & 0xFF
+                    if checksum & 0xFF == self._read_buffer[-1]:
+                        self.data = self._read_buffer
+                        self._read_buffer = bytes()
                         return True
                     else:
-                        self.read_buffer = bytes()
+                        self._read_buffer = bytes()
                         return False
         return False
 
     @property
-    def rx_data(self):
-        return self.read_save_buffer
+    def rx_data(self) -> bytes:
+        """
+        读取接收数据。
+
+        Returns:
+            bytes: 接收到的数据。
+        """
+        return self.data
 
     def close(self):
-        if self.ser != None:
+        """
+        关闭串口连接。
+
+        Returns:
+            None
+        """
+        if self.ser is not None:
             self.ser.close()
             self.ser = None
 
     def write(self, data: bytes):
-        data = copy(data)
+        """
+        向串口写入数据。
+
+        Args:
+            data (bytes): 要写入的数据。
+
+        Returns:
+            bytes: 实际写入的数据。
+        """
         if isinstance(data, list):
             data = bytes(data)
         if not isinstance(data, bytes):
             raise TypeError("data must be bytes")
-        len_as_byte = len(data).to_bytes(1, self.byte_order)
-        send_data = bytes(self.sned_start_bit) + bytes(self.send_option_bit) + len_as_byte + data
+        len_as_byte = len(data).to_bytes(1, self._byte_order)
+        send_data = self._send_head + len_as_byte + data
         checksum = 0
         for i in range(0, len(send_data)):
             checksum += send_data[i]
             checksum &= 0xFF
-        send_data += checksum.to_bytes(1, self.byte_order)
+        send_data += checksum.to_bytes(1, self._byte_order)
         self.ser.write(send_data)
         self.ser.flush()
         return send_data
